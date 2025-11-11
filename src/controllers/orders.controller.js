78872ec0039d383ebbfcab12c936e65aabbd2557
chichilu-01 <hoangminhphuong270401@gmail.com>
2025-11-11 06:34:16 +0000

    H

--- src/controllers/orders.controller.js
+++ src/controllers/orders.controller.js
@@ -169,7 +169,7 @@ export const updateOrderStatus = async (req, res) => {
     const validStatuses = [
       "pending",
       "confirmed",
-      "shipped",
+      "shipping",
       "completed",
       "cancelled",
     ];
